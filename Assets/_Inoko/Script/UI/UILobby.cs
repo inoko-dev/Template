644ed880156a16421d868120c5b5a5b4f9507439
inoko_dev <inoko.dev.2k1@gmail.com>
2024-10-11 20:06:43 +0700

    + import textmeshpro

--- Assets/_Inoko/Script/UI/UILobby.cs
+++ Assets/_Inoko/Script/UI/UILobby.cs
@@ -9,19 +9,11 @@ namespace InnoStudio
     public class UILobby : UICanvas
     {
         [FoldoutGroup("Button")]
-        [SerializeField] private Button btnClose;
-        [FoldoutGroup("Button")]
         [SerializeField] private Button btnPlay;
 
         public void Init()
         {
-            btnClose.onClick.AddListener(OnBtnClose);
             btnPlay.onClick.AddListener(OnBtnPlay);
-        }
-
-        public override void OnBtnClose()
-        {
-            base.OnBtnClose();
         }
 
         public void OnBtnPlay()
